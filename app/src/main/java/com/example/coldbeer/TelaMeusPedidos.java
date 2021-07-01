4c53da1c83297cfedfc2b22f72c6996175cfbaf4
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-07-01 12:16:59 -0300

    tela meus pedidos

---
+++ app/src/main/java/com/example/coldbeer/TelaMeusPedidos.java
@@ -0,0 +1,14 @@
+package com.example.coldbeer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaMeusPedidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_meus_pedidos);
+    }
+}
